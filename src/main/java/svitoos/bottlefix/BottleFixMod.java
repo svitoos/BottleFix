2fbb7b691b609ee55a1782e25b234bf28182d212
svitoos <svitoos@gmail.com>
2019-09-11 15:11:16 +0300

    update to mc shapshot 19w36a
    fix glass bottle dispenser behavior

--- src/main/java/svitoos/bottlefix/BottleFixMod.java
+++ src/main/java/svitoos/bottlefix/BottleFixMod.java
@@ -1,7 +1,26 @@
 package svitoos.bottlefix;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.BeeHiveBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.FluidDrainable;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
+import net.minecraft.block.entity.DispenserBlockEntity;
+import net.minecraft.fluid.BaseFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 
+@SuppressWarnings("unused")
 public class BottleFixMod implements ModInitializer {
   @Override
   public void onInitialize() {
@@ -9,5 +28,57 @@ public class BottleFixMod implements ModInitializer {
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
 
+    DispenserBlock.registerBehavior(
+        Items.GLASS_BOTTLE.asItem(),
+        new ItemDispenserBehavior() {
+          private final ItemDispenserBehavior fallback = new ItemDispenserBehavior();
+
+          private ItemStack fill(
+              BlockPointer blockPointer, ItemStack itemStack, ItemStack filledItemStack) {
+            itemStack.decrement(1);
+            if (itemStack.isEmpty()) {
+              return filledItemStack.copy();
+            } else {
+              if (((DispenserBlockEntity) blockPointer.getBlockEntity())
+                      .addToFirstFreeSlot(filledItemStack.copy())
+                  < 0) {
+                this.fallback.dispense(blockPointer, filledItemStack.copy());
+              }
+
+              return itemStack;
+            }
+          }
+
+          public ItemStack dispenseSilently(BlockPointer blockPointer, ItemStack itemStack) {
+            IWorld world = blockPointer.getWorld();
+            BlockPos blockPos =
+                blockPointer
+                    .getBlockPos()
+                    .offset(blockPointer.getBlockState().get(DispenserBlock.FACING));
+            BlockState blockState = world.getBlockState(blockPos);
+            Block block = blockState.getBlock();
+
+            if (block.matches(BlockTags.BEEHIVES)
+                && blockState.get(BeeHiveBlock.HONEY_LEVEL) >= 5) {
+              ((BeeHiveBlock) blockState.getBlock())
+                  .emptyHoney(world.getWorld(), blockState, blockPos, null);
+              return this.fill(
+                  blockPointer, itemStack, new ItemStack(Items.HONEY_BOTTLE));
+            } else {
+              if (world.getFluidState(blockPos).matches(FluidTags.WATER)
+                  && block instanceof FluidDrainable) {
+                Fluid fluid =
+                    ((FluidDrainable) block).tryDrainFluid(world, blockPos, blockState);
+                if (fluid instanceof BaseFluid) {
+                  return this.fill(
+                      blockPointer,
+                      itemStack,
+                      PotionUtil.setPotion(new ItemStack(Items.POTION), Potions.WATER));
+                }
+              }
+              return itemStack;
+            }
+          }
+        });
   }
 }
